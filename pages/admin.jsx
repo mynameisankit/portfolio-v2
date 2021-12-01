6f4fa88fd3d595405c52484a6336cc4ba6afcb97
Ankit Kumar <ankit.kumar19@iiitg.ac.in>
2021-12-01 14:26:01 +0530

    Added Netlify CMS

---
+++ pages/admin.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Head from 'next/head';
+import dynamic from 'next/dynamic';
+import config from '../cms/config';
+
+const CMS = dynamic(() =>
+    import('netlify-cms-app').then((cms) => {
+        cms.init({ config });
+    }),
+    { ssr: false, loading: () => <p>Loading...</p> }
+);
+
+function AdminPage(props) {
+    return (
+        <React.Fragment>
+            <Head>
+                <title>Portfolio v2 | Admin Panel</title>
+            </Head>
+            <CMS />
+        </React.Fragment>
+    );
+};
+
+export default AdminPage;
